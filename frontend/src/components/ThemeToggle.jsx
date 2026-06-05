3350833c366e55930900402a359606bdc5544610
Vinayak Suryawanshi <vinayak.suryawanshi@irisbusiness.com>
2026-06-05 18:15:07 +0530

    adding theam dark and light theam

---
+++ frontend/src/components/ThemeToggle.jsx
@@ -0,0 +1,13 @@
+import { useTheme } from "../context/ThemeContext";
+
+function ThemeToggle() {
+    const { theme, toggleTheme } = useTheme();
+
+    return (
+        <button onClick={toggleTheme}>
+            {theme === "light" ? "🌙 Dark" : "☀️ Light"}
+        </button>
+    );
+}
+
+export default ThemeToggle;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import { ThemeProvider } from "./context/ThemeContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    // <ThemeProvider>
+    //     <App />
+    // </ThemeProvider>
     <App />
-  </React.StrictMode>,
-)
+);
